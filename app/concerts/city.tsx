19ff34e56b7c151b4103215da82e3679e53f1f51
coder7475 <robiulhossain7475@gmail.com>
2024-12-06 11:12:22 +0600

    feat: load data in dynamic routes

--- app/concerts/city.tsx
+++ app/concerts/city.tsx
@@ -1,6 +1,14 @@
 // route: concerts/:city
-const City = () => {
-  return <div>This is the dynamic city</div>;
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  return params.city;
+};
+const CityUnderConcerts = () => {
+  const city = useLoaderData<typeof loader>();
+  console.log(city);
+  return <div>Dynamic City: {city} under concerts</div>;
 };
 
-export default City;
+export default CityUnderConcerts;
